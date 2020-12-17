e5bbab2d9f9b19bf98e7b0aa9ffe6a0345632552
Jeffrey Lee <jglee1@live.com>
2020-12-17 10:30:24 -0800

    Added REST for profile and images

--- server.js
+++ server.js
@@ -31,11 +31,11 @@ const database = {
 
 //routes
 app.get('/', (req, res) => {
-  res.send('this is working')
+  res.send(database.users)
 })
 
 //signin --> POST = success/fail
-app.post('/signin', (req,res) => {
+app.post('/signin', (req, res) => {
   if(req.body.email === database.users[0].email && req.body.password === database.users[0].password) {
     res.json('success');
   }
@@ -46,10 +46,48 @@ app.post('/signin', (req,res) => {
 
 //register --> user
 app.post('/register', (req, res) => {
+  const { email, name, password } = req.body;
+  database.users.push({
+    id: '125',
+    name: name,
+    email: email,
+    password: 'cookies',
+    entries: 0,
+    joined: new Date()
+  })
+  res.json(database.users[database.users.length - 1])
 })
 
 //profile/:userID --> GET = user
+app.get('/profile/:id', (req, res) => {
+  const { id } = req.params;
+  let found =  false;
+  database.users.forEach(user =>  {
+    if(user.id === id) {
+      found = true;
+      return res.json(user);
+    }
+  })
+  if(!found) {
+    res.status(404).json('no such user');
+  }
+})
+
 //image --> PUT --> user
+app.post('/image', (req, res) => {
+  const { id } = req.body;
+  let found =  false;
+  database.users.forEach(user =>  {
+    if(user.id === id) {
+      found = true;
+      user.entries++;
+      return res.json(user.entries);
+    }
+  })
+  if(!found) {
+    res.status(404).json('no such user');
+  }
+})
 
 app.listen(port, () => {
   console.log(`app is running on port ${port}`);

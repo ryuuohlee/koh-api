e74a0c1478e7e1ca06970c5741de6d4ce1be5dfb
Jeffrey Lee <jglee1@live.com>
2020-12-17 09:48:26 -0800

    Testing sign in post

--- server.js
+++ server.js
@@ -1,12 +1,52 @@
 const express = require('express');
+const bodyParser = require('body-parser');
+
 
 const app = express();
 const port = 3000;
 
+//middleware
+app.use(bodyParser.json());
+
+const database = {
+  users: [
+    {
+      id: '123',
+      name: 'John',
+      email: 'john@gmail.com',
+      password: 'cookies',
+      entries: 0,
+      joined: new Date()
+    },
+    {
+      id: '124',
+      name: 'Sally',
+      email: 'sally@gmail.com',
+      password: 'bananas',
+      entries: 0,
+      joined: new Date()
+    }
+  ]
+}
+
 //routes
 app.get('/', (req, res) => {
-  res.send('this is working');
+  if(req.body.email === database.users[0].email && req.body.password === database.users[0].password) {
+    res.json('success');
+  }
+  else {
+    res.status(400).json('error loggin in')
+  }
 })
+
+//signin --> POST = success/fail
+app.post('/signin', (req,res) => {
+  res.send('signing')
+})
+
+//register --> user
+//profile/:userID --> GET = user
+//image --> PUT --> user
 
 app.listen(port, () => {
   console.log(`app is running on port ${port}`);

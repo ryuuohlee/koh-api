04409d62dd60d32441b3da9639f9a98c6d2ba53c
Jeffrey Lee <jglee1@live.com>
2021-04-02 20:15:57 -0700

    Feature: created controller section!

--- server.js
+++ server.js
@@ -1,15 +1,21 @@
+require('dotenv').config();
 const express = require('express');
 const bodyParser = require('body-parser');
 const bcrypt = require('bcryptjs');
 const cors = require('cors');
 const knex = require('knex')
 
+const register = require('./controllers/register.js');
+const signin = require('./controllers/signin.js');
+const profile = require('./controllers/profile.js');
+const image = require('./controllers/image.js');
+
 const db = knex({
   client: 'pg',
   connection: {
     host : '127.0.0.1',
     user : 'postgres',
-    password : '1234',
+    password : process.env.PASSWORD,
     database : 'koh'
   }
 });
@@ -22,86 +28,19 @@ app.use(bodyParser.json());
 app.use(cors());
 
 //routes
-app.get('/', (req, res) => {
-  res.send('success')
-})
+app.get('/', (req, res) => { res.send('success') });
 
 //signin --> POST = success/fail
-app.post('/signin', (req, res) => {
-  db.select('email', 'hash').from('login')
-    .where('email', '=', req.body.email)
-    .then(data => {
-      const isValid = bcrypt.compareSync(req.body.password, data[0].hash);
-      if(isValid) {
-        return db.select('*').from('users')
-          .where('email', '=', req.body.email)
-          .then(user => {
-            res.json(user[0])
-          })
-          .catch(err => res.status(400).json('unable to get user'));
-      }
-      else {
-        res.status(400).json('wrong credentials');
-      }
-    })
-    .catch(err => res.status(400).json('wrong credentials'));
-})
+app.post('/signin', (req, res) => { signin.handleSignin(req, res, db, bcrypt)});
 
 //register --> user
-app.post('/register', (req, res) => {
-  const { email, name, password } = req.body;
-  var hash = bcrypt.hashSync(password);
-  db.transaction(trx => {
-    trx.insert({
-      hash: hash,
-      email: email
-    })
-    .into('login')
-    .returning('email')
-    .then(loginEmail => {
-      return trx('users')
-          .returning('*')
-          .insert({
-          email: loginEmail[0],
-          name: name,
-          joined: new Date()
-        })
-          .then(user => {
-            res.json(user[0]);
-          })
-        })
-        .then(trx.commit)
-        .catch(trx.rollback)
-      })
-      .catch(err => res.status(400).json('unable to register'));
-  })
+app.post('/register', (req, res) => { register.handleRegister(req, res, db, bcrypt) })
 
 //profile/:userID --> GET = user
-app.get('/profile/:id', (req, res) => {
-  const { id } = req.params;
-  db.select('*').from('users').where({id})
-    .then(user => {
-      if(user.length) {
-        res.json(user[0]);
-      }
-      else {
-        res.status(400).json('Not found')
-      }
-  })
-    .catch(err => res.status(400).json('error getting user'));
-})
+app.get('/profile/:id', (req, res) => { profile.handleProfile(req, res, db) })
 
 //image --> PUT --> user
-app.put('/image', (req, res) => {
-  const { id } = req.body;
-  db('users').where('id', '=', id)
-    .increment('entries', 1)
-    .returning('entries')
-    .then(entries => {
-      res.json(entries[0])
-    })
-    .catch(err => res.status(400).json('unable to get entries'))
-})
+app.put('/image', (req, res) => { image.handleImage(req, res, db)})
 
 app.listen(port, () => {
   console.log(`app is running on port ${port}`);

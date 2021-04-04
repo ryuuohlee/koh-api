bdda194a68f4d87f3c1a4948c9b1959f437aca60
Jeffrey Lee <jglee1@live.com>
2021-04-03 21:02:38 -0700

    Feature: connecting to heroku pg db

--- server.js
+++ server.js
@@ -13,7 +13,7 @@ const image = require('./controllers/image.js');
 const db = knex({
   client: 'pg',
   connection: {
-    host : '127.0.0.1',
+    host : 'postgresql-metric-87874',
     user : process.env.USER,
     password : process.env.PASSWORD,
     database : 'koh'
